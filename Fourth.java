73633b916f68b5536811145576c027a56840980c
nora <kertesznora.kn@gmail.com>
2023-03-16 14:53:34 +0100

    SGL 16.03

---
+++ Fourth.java
@@ -0,0 +1,20 @@
+public class Fourth {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
+/*Schreiben Sie ein Java-Programm, das den Benutzer auffordert, einen Benutzernamen
+und ein Kennwort einzugeben. Das Programm sollte den Benutzer wiederholt zur
+Eingabe eines Benutzernamens und eines Passworts aufzufordern, bis ein korrekter
+Benutzername und ein korrektes Passwort eingegeben werden oder die maximale
+Anzahl der Versuche erreicht ist.
+Der richtige Benutzername ist "user123" und das richtige Passwort ist "password123".
+Wenn der Benutzer falsche Anmeldedaten eingibt, sollte das Programm die Meldung
+"Falscher Benutzername oder falsches Passwort. Bitte versuchen Sie es erneut." Wenn
+die maximale Anzahl von Versuchen erreicht ist, sollte das Programm die Meldung
+"Maximale Anzahl von Versuchen erreicht. Programm beenden." und das Programm
+beenden. */
