948f577dc561a781e3b7571f453d6256e26d2b3f
Nora <kertesznora.kn@gmail.com>
2023-03-19 15:36:15 +0100

    Wochenende

--- task.java
+++ task.java
@@ -1,7 +1,15 @@
-public class task {
+import java.util.Arrays;
+
+public class task { // ARRAY
 
     public static void main(String[] args) {
 
+        int[] firstArray = new int[]{1, 3, 3, 4, 5};
+
+        for (int i = 0; i < firstArray.length; i++) {
+
+            System.out.println(firstArray[i]);
+        }
 
 
     }
