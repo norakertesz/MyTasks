0f242284a936eef21f68e95e22f5cb920a598a18
Nora <kertesznora.kn@gmail.com>
2023-03-19 15:45:16 +0100

    Wochenende

--- task.java
+++ task.java
@@ -8,9 +8,21 @@ public class task { // ARRAY
 
         for (int i = 0; i < firstArray.length; i++) {
 
-            System.out.println(firstArray[i]);
         }
 
+        //------------------------------------------------------
+        double[] secondArray= new double[]{1.1,3.5,2.7,9.9,15.2,98.21};
+        //Schreibe ein Programm, das ein Array von 5 Gleitkommazahlen erzeugt (Kein
+        //Einlesen aus der Konsole) und das größte Element im Array findet und ausgibt.
 
+
+    double max = secondArray[0];
+    for (int i=1; i<secondArray.length; i++) {
+        if (secondArray[i]>max) {
+            max = secondArray[i];
+        }
+
+}
+        System.out.println(max);
     }
 }
