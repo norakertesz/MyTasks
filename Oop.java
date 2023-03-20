3da5f3ce171cfb0338469fd53bedbd95fda0b482
nora <kertesznora.kn@gmail.com>
2023-03-20 11:25:49 +0100

    20.03.23

---
+++ Oop.java
@@ -0,0 +1,14 @@
+public class Oop {
+    String name;
+    int alter;
+    String farbe;
+    String rasse;
+String bellt="Wuff";
+
+    public String hatErGebellt() {
+        return bellt;
+    }
+    public boolean stock(){
+        return false;
+    }
+}
