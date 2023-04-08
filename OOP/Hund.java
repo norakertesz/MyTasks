43f333e74e3b85591033f0f5aab7468c09974f75
Nora <kertesznora.kn@gmail.com>
2023-04-08 20:47:57 +0200

    08.04

---
+++ OOP/Hund.java
@@ -0,0 +1,22 @@
+package OOP;
+
+public class Hund {
+
+    int alter;
+    String art;
+    String fellfarbe;
+    boolean kannPfotenGeben;
+
+
+public void bellen(){
+
+    System.out.println("Wuff-Wuff");
+
+}
+public int plusRechnen(int a, int b){
+    return a+b;
+
+}
+
+
+}
---
+++ OOP/HundMain.java
@@ -0,0 +1,15 @@
+package OOP;
+
+import java.util.Scanner;
+
+public class HundMain {
+
+    public static void main(String[] args) {
+        Hund hund1 = new Hund();
+
+        hund1.bellen();
+        Hund hund2 = new Hund();
+        System.out.println(hund2.plusRechnen(2, 2));
+
+    }
+}
